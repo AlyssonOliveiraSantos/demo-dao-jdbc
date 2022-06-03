997ae03dfba920a54b38cb3e1b2e4f2b5f60a7eb
Alysson <alyssonn.bra@live.com>
2022-06-03 15:11:59 -0300

    Seller entity class

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,6 +1,8 @@
 package program;
 
+import java.util.Date;
 import model.entites.Department;
+import model.entites.Seller;
 
 
 
@@ -10,7 +12,8 @@ public class Program {
 
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        System.out.println(seller);
 
     }
 
