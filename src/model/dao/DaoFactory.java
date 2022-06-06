90d74b6f58378e1d21f8e72a401041df45b4a406
Alysson <alyssonn.bra@live.com>
2022-06-06 16:13:39 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 
@@ -8,7 +9,7 @@ public class DaoFactory {
     
     public static SellerDao createSellerDao(){
     
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     
     }
     
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,18 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.dao.impl;
 
+
+import db.DB;
 import java.util.List;
 import model.dao.SellerDao;
 import model.entites.Seller;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.entites.Department;
+import java.sql.Connection;
 
 /**
  *
  * @author b246131
  */
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -31,12 +40,47 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public Seller findById(Integer Id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    +"FROM seller INNER JOIN department "
+                    +"ON seller.DepartmentId = department.Id "
+                    +"WHERE seller.Id = ?");
+            
+            st.setInt(1, Id);
+            rs = st.executeQuery();
+            
+            if(rs.next()){
+                Department dept = new Department();
+                dept.setId(rs.getInt("DepartmentId"));
+                dept.setName(rs.getString("DepName"));
+                
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dept);
+                return obj;
+            }
+            return null;
+        }catch(SQLException e){
+                    throw new db.DbIntegrityException("Error findBy Seller: " + e);
+                    }
+        finally{
+        DB.closeResultSet(rs);
+        DB.closeStatement(st);
+        }
+
     }
 
     @Override
     public List<Seller> findAll() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
