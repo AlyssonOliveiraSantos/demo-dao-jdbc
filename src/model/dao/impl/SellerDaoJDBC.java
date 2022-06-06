8fcc9433bde347c46624532b38430ae6e12a3d7a
Alysson <alyssonn.bra@live.com>
2022-06-06 17:05:24 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,6 +1,5 @@
 package model.dao.impl;
 
-
 import db.DB;
 import java.util.List;
 import model.dao.SellerDao;
@@ -10,8 +9,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import model.entites.Department;
 import java.sql.Connection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -48,26 +48,25 @@ public class SellerDaoJDBC implements SellerDao {
         try {
             st = conn.prepareStatement(
                     "SELECT seller.*,department.Name as DepName "
-                    +"FROM seller INNER JOIN department "
-                    +"ON seller.DepartmentId = department.Id "
-                    +"WHERE seller.Id = ?");
-            
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
+
             st.setInt(1, Id);
             rs = st.executeQuery();
-            
-            if(rs.next()){
+
+            if (rs.next()) {
                 Department dept = instancianteDepartment(rs);
-                
+
                 Seller obj = instancianteSeller(rs, dept);
                 return obj;
             }
             return null;
-        }catch(SQLException e){
-                    throw new db.DbIntegrityException("Error findBy Seller: " + e);
-                    }
-        finally{
-        DB.closeResultSet(rs);
-        DB.closeStatement(st);
+        } catch (SQLException e) {
+            throw new db.DbIntegrityException("Error findBy Seller: " + e);
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
         }
 
     }
@@ -78,22 +77,63 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     private Department instancianteDepartment(ResultSet rs) throws SQLException {
-            Department dept = new Department();
-            dept.setId(rs.getInt("DepartmentId"));
-            dept.setName(rs.getString("DepName"));
-            return dept;
-        
+        Department dept = new Department();
+        dept.setId(rs.getInt("DepartmentId"));
+        dept.setName(rs.getString("DepName"));
+        return dept;
+
     }
 
-    private Seller instancianteSeller(ResultSet rs, Department dept) throws SQLException{
-      Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dept); 
-                return obj;
+    private Seller instancianteSeller(ResultSet rs, Department dept) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dept);
+        return obj;
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE DepartmentId = ? "
+                    + "ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dept = map.get(rs.getInt("DepartmentId"));
+                
+                if (dept == null) {
+                    dept = instancianteDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dept);
+
+                }
+                Seller obj = instancianteSeller(rs, dept);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new db.DbIntegrityException("Error findBy Seller: " + e);
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+        }
+
     }
 
 }
--- src/program/Program.java
+++ src/program/Program.java
@@ -1,6 +1,7 @@
 package program;
 
 import java.util.Date;
+import java.util.List;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entites.Department;
@@ -16,6 +17,15 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        
+        
+         System.out.println("\n===== TEST 2: seller findByDepartment =====");
+         Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);list.get(1);
+        
+        for(Seller obj: list){
+        System.out.println(obj);
+        }
     }
 
 }
