8b4961070d7bc58378581ffab3019c5721f928d1
Alysson <alyssonn.bra@live.com>
2022-06-07 12:53:24 -0300

    DepartmentDaoJDBC class implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,6 +2,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 
@@ -10,6 +11,12 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
     
         return new SellerDaoJDBC(DB.getConnection());
+    
+    }
+    
+    public static DepartmentDao createDepartmentDao(){
+    
+        return new DepartmentDaoJDBC(DB.getConnection());
     
     }
     
---
+++ src/program/NewMain.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package program;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
+import model.entites.Department;
+import model.entites.Seller;
+
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("===== TEST 1: Department Insert =====");
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Department department = new Department (null, "Music");
+        departmentDao.insert(department);
+        System.out.println(department);
+        
+         System.out.println("===== TEST 2: Department update =====");
+         department = departmentDao.findById(6);
+         department.setName("Computers");
+        departmentDao.update(department);
+        System.out.println(department);
+        
+        System.out.println("===== TEST 3: Department FindById =====");
+        department = departmentDao.findById(2);
+        System.out.println(department);
+        
+        
+        System.out.println("===== TEST 3: Department FindById =====");
+        List<Department> list = departmentDao.findAll();
+        System.out.println(list);
+        
+        
+    }
+    
+}
